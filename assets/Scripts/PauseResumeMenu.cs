c5aa470efd3cd6397f1be059b8888d6702dde154
Lucy Lee <leelucy412@gmail.com>
2022-05-20 10:56:28 -0400

    Add comments and clean up whitespace

--- assets/Scripts/PauseResumeMenu.cs
+++ assets/Scripts/PauseResumeMenu.cs
@@ -9,15 +9,11 @@ public class PauseResumeMenu : MonoBehaviour
     [SerializeField] GameObject[] pauseMode;
     [SerializeField] GameObject[] playMode;
 
-
-
-
-
-
     void Awake()
     {
         pauseMode = GameObject.FindGameObjectsWithTag("Pause Mode");
         playMode = GameObject.FindGameObjectsWithTag("Play Mode");
+
         foreach (GameObject g in playMode)
         {
             g.SetActive(true);
@@ -27,53 +23,57 @@ public class PauseResumeMenu : MonoBehaviour
             g.SetActive(true);
         }
     }
+
     void Start()
     {
-        
         foreach (GameObject g in pauseMode)
         {
-            g.SetActive(false);
-        }
+            g.SetActive(false); // Pause mode starts off and remains inactive until
+        }                       // player presses pause button
    }
 
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
     }
+
     void Update()
     {
         if (GameIsPaused == true)
         {
-            AudioListener.volume = 0.0f;
-
+            AudioListener.volume = 0.0f; // Mute audio on pause
         }
 
         else
         {
             AudioListener.volume = 1.0f;
-
         }
     }
     public void Resume()
     {
+        // Re-enable play mode
         foreach (GameObject g in playMode)
         {
             g.SetActive(true);
         }
+        // Disable pause mode
         foreach (GameObject g in pauseMode)
         {
             g.SetActive(false);
         }
+
         Time.timeScale = 1.0f;
         GameIsPaused = false;
-
     }
+
     public void Pause()
     {
+        // Re-enable pause mode
         foreach (GameObject g in pauseMode)
         {
             g.SetActive(true);
         }
+        // Disable play mode
         foreach (GameObject g in playMode)
         {
             g.SetActive(false);
@@ -94,6 +94,4 @@ public class PauseResumeMenu : MonoBehaviour
         Debug.Log("Quitting game...");
         Application.Quit();
     }
-
-
 }
